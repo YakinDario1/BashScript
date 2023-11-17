a66fc8ed7b61bc4879e1d06c540aca3d51f0ec4f
Yakin Dario <yakin_d.m_15@hotmail.com>
2023-11-16 18:03:30 -0600

    add: Uso de parametro

---
+++ ejercicio/p1.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Un script puede recibir información mediante parámetro, que serán cadenas de texto que pondremos después del nombre del script   
+
+echo El primer parametro es $1
+echo El numero de parametros ha sido $#
+echo Todos los parametros son: $*
