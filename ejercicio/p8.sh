e9297139ac6a5e044192e4cbbe2d37ba6aba3d9c
Yakin Dario <yakin_d.m_15@hotmail.com>
2023-11-16 18:08:43 -0600

    add: Operaciones

---
+++ ejercicio/p8.sh
@@ -0,0 +1,9 @@
+
+#! /bin/bash
+read -p "Indroduzca el primer numero: " n1
+read -p "Introduzca el segundo numero: " n2
+echo "La suma es $((n1+n2))"
+echo "La Resta es $((n1-n2))"
+echo "La Multiplicacion es $((n1*n2))"
+echo "La Divicion es $((n1/n2))"
+echo "La Modulo es $((n1%n2))"
