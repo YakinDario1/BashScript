1621089745b7bd7cf71f552d17264666203b6ede
Yakin Dario <yakin_d.m_15@hotmail.com>
2023-11-16 18:04:51 -0600

    add: Ejercicio de paramentro

---
+++ ejercicio/p3.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+echo el script $0 
+echo Lo esta en ejecucion $USER
+echo Y se le ha asginado el PID:$$
+echo Usamos el paramentro $1 
+
+mensaje="Hola Mundo"
+
+echo Para decir... $mensaje
